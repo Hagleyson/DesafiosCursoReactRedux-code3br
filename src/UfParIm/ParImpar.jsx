c1d928707a361d43f042069e10ef378b013c9c07
hagleyson <hagleyson@gmail.com>
2021-01-30 20:57:35 -0300

    melhorando

--- src/UfParIm/ParImpar.jsx
+++ src/UfParIm/ParImpar.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react"
-function calcular(n){
-    if(n%2 === 0 ) return "Par"
-    if(n%2 === 1) return "Impar"
-}
 export default function ParImpar () {
     const [number,setNumber] = useState(0)
     const [type,setType] = useState("par")
     useEffect(()=>{
-        setType(calcular(number))
+        setType(number % 2 === 0 ? "Par" : "Impar")
     },[number,setType])
     return(
     <div>
